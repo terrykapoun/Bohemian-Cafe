908361631f20574b9c5640181c44675071197bc8
terrykapoun <terry.kapoun@gmai.com>
2019-05-08 09:27:41 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MenuComponent } from './menu/menu.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {path: 'home', component: HomePageComponent},
   {path: 'menu', component: MenuComponent},
   {path: 'checkout', component: CheckoutComponent},
+  {path: 'registration', component: RegistrationComponent},
   {path: '**', redirectTo: 'home'}];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,22 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-
 import { HomePageComponent } from './home-page/home-page.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    
+
     HomePageComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,ReactiveFormsModule 
+    AppRoutingModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
