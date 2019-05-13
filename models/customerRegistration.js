276c95b27cc191adcecb5d9e5cc2df61792350bb
terrykapoun <terry.kapoun@gmai.com>
2019-05-13 15:55:24 -0500

    updates

--- models/customerRegistration.js
+++ models/customerRegistration.js
@@ -3,15 +3,16 @@ const bcrypt = require("bcrypt")
 
 function create(req, res) {
     console.log(req.body)
-    pool.query("SELECT * FROM BOHEMIANCAFEDATABASE WHERE email = ?",
+    pool.query("SELECT * FROM BohemianCafeTable WHERE email = ?",
         [req.body.email], (err, querryReturn) => {
+            console.log(err)
             if (querryReturn[0]) {
                 return res.send("EMAIL ALREADY EXIST")
 
             }
             let password = bcrypt.hashSync(req.body.password, 5);
             let email = req.body.email;
-            pool.query("INSERT INTO BOHEMIANCAFEDATABASE (firstName, lastName, phone, streetAddress, city, state, zip, email, password) VALUES(?,?,?,?,?,?,?,?,?)", [req.body.firstName, req.body.lastName, req.body.phoneNumber, req.body.streetAddress, req.body.city, req.body.state, req.body.zip, req.body.email, req.body.password], (err, result) => {
+            pool.query("INSERT INTO BohemianCafeTable (firstName, lastName, phone, streetAddress, city, state, zip, email, password) VALUES(?,?,?,?,?,?,?,?,?)", [req.body.firstName, req.body.lastName, req.body.phoneNumber, req.body.streetAddress, req.body.city, req.body.state, req.body.zip, req.body.email, password], (err, result) => {
                 if (!err) {
                     return res.send({msg:"Signed Up!"})
                 }
