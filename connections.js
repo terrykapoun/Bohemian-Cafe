878e00318dbc1bac5dd5b430e646f7cac18fd8ab
terrykapoun <terry.kapoun@gmai.com>
2019-05-13 14:59:42 -0500

    first commit

--- connections.js
+++ connections.js
@@ -3,9 +3,9 @@ const mysql = require("mysql");
 let pool = mysql.createPool({
   connectionLimit: 10,
   Port: 3306,
-  host: process.env.DBhost,
-  user: process.env.DBuser,
-  password: process.env.DBpassword,
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
   database: process.env.DB,
 });
   
